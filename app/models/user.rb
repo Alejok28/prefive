61514dd360c44befdf561a6fb6101127f9844691
Alejok28 <alejo_k28@hotmail.com>
2018-02-06 10:21:09 -0500

    Heroku Config

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@
 #
 
 class User < ApplicationRecord
-  has_many :posts
+  has_many :posts, dependent: :delete_all
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
