dd265cbd3aff5bd39fbc4c5ba54eeba36f182dff
Alejok28 <alejo_k28@hotmail.com>
2018-02-06 15:20:44 -0500

    Changes flash messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Tu publicación fue eliminada exitosamente.' }
+        format.html { redirect_to @post, notice: 'Tu publicación fue editada exitosamente.' }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit }
@@ -46,7 +46,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.html { redirect_to posts_url, notice: 'Tu publicación fue eliminada exitosamente.' }
       format.json { head :no_content }
     end
   end
