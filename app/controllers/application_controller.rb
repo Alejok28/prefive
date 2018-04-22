b2e3748ac5d97c923d1146164ade77f4638a5b5a
Alejok28 <alejo_k28@hotmail.com>
2018-04-21 20:45:26 -0500

    Changes devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :country, :city, :college])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :country, :city, :college])
   end
 end
